5435ea9405f6d307d4b963182d03ab0aa20fda53
Vitor Rocha <65897710+Vitorrocha98@users.noreply.github.com>
2023-03-24 02:09:28 -0300

    Atualização no banner, na seção 'sobre' e estilos em geral.

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ formSubmit.onclick = function(){
     contactThanks.classList.remove('hidden-message');
     contactThanks.classList.add('active-message');
 }
+
+//========================================= Animação do banner
+
+if("IntersectionObserver" in window) {
+    let handleIntersection = (entries, observer) => {
+        entries.forEach(entry =>{
+            if (entry.isIntersecting){
+                let item = entry.target;
+                if(item.id == "banner-principal"){
+                    //console.log("======== VISUALIZOU BANNER ========")
+                    document.querySelector('#banner-principal .titulo-apresentacao').classList.add('active-banner');
+                }
+                else if(item.id == "experience-target"){
+                    //console.log("VISUALIZOU EXPERIENCIA")
+                    document.querySelector('#banner-principal .titulo-apresentacao').classList.remove('active-banner');
+                }
+            }
+        })
+    }
+
+    let config = {threshold:0.5};
+
+    let observer = new IntersectionObserver(handleIntersection, config);
+
+    let itensObservados = [].slice.call(document.querySelectorAll('#banner-principal, #experience-target'));
+    itensObservados.forEach(item => observer.observe(item));
+}
+
+//=========================================
